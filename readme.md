f87b0a2b9da9bed036459c9a9f8534c6e86f19d5
Soo Oh <soo.oh5@gmail.com>
2022-02-10 13:24:28 -0800

    updated readme and details in setup

--- readme.md
+++ readme.md
@@ -1,1 +1,39 @@
 # Reveal's Altair theme
+
+A Reveal theme for [altair](https://altair-viz.github.io/).
+
+With much gratitude to the [Los Angeles Times](https://github.com/datadesk/altair-latimes).
+
+
+## Instructions
+
+After setting up a Python virtualenv, install from PyPI:
+
+```sh
+% pip install altair-reveal
+```
+
+Import theme with Altair, then register and enable theme.
+
+```python
+import altair as alt
+import altair_reveal as reveal
+
+alt.themes.register('reveal', reveal.theme)
+alt.themes.enable('reveal')
+```
+
+View and use a (limited) color palette.
+
+```python
+altair_reveal.palette
+```
+
+## Examples
+
+Check out [the `examples` notebook](examples.ipynb) for examples of charts, including customizations.
+
+## Fonts
+
+System-wide 'Tenon' font installation required.
+
--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ from setuptools import setup, find_packages
 # Setting up
 setup(
         name='altair_reveal', 
-        version='0.0.1',
+        version='0.0.2',
         author='Soo Oh',
         author_email='soh@revealnews.org',
         description='Reveal theme for Altair library',
+        long_description='Reveal theme for the Altair statistical visualization library.',
+        long_description_content_type='text/x-rst',
         url='http://www.github.com/cirlabs/altair-theme-reveal',
         license='MIT',
         packages=find_packages(),
         install_requires=['altair'],
         keywords=['python', 'altair', 'theme', 'cir', 'reveal', 'investigative'],
         classifiers= [
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Education',
-            'Programming Language :: Python :: 2',
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Other Audience',
             'Programming Language :: Python :: 3',
             'Operating System :: MacOS :: MacOS X',
-            'Operating System :: Microsoft :: Windows',
             'License :: OSI Approved :: MIT License',
         ]
 )
